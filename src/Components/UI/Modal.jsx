b7ce560a2ecdb074bfac03a9daea676e54de9445
AntCannon <anthonycannonier@gmail.com>
2024-07-28 09:41:57 -0400

    add update profile modal

---
+++ src/Components/UI/Modal.jsx
@@ -0,0 +1,23 @@
+import './Modal.css'
+import { useState } from 'react'
+
+
+export default function Modal({
+  showAlert,
+  message,
+  handleAlertClose
+}) {
+
+  console.log('modal', showAlert)
+
+  return (
+    <div
+      className="modal poppins"
+      style={{display: showAlert ? "flex" : "none"}}
+    
+    >
+      <h3 className="message">{message}</h3>
+      <button className="btn_bg_light" onClick={handleAlertClose}>Close</button>
+    </div>
+  )
+}
